368e06aa28e1da8f39cfd69d2ce3a2a1947f7703
Augusto Maced <macedo271203@gmail.com>
2025-02-03 21:58:57 -0300

    aula dia 03/02

--- tipodeatividade/models.py
+++ tipodeatividade/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.urls import reverse 
 
 # Create your models here.
+class TipoDeAtividade(models.Model):
+    """Modeo representando um tipo de Atividade"""
+    codigo = models.IntegerField(primary_key=True,
+                                 help_text='Código do tipo de atividade')
+    descricao= models.CharField(max_length=70, null=False,
+                                help_text='Informe a descrição do tipo de atividade')
+    
+    def __str__(self):
+        return f'{self.codigo}{self.descricao}'
--- tipodeatividade/views.py
+++ tipodeatividade/views.py
@@ -3,8 +3,12 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<!DOCType='html'><html><body><p>Olá, estou no App 'Tipo de Atividade'</p></body></html>")
+    return HttpResponse("<!DOCType html><html><body><p>Olá, estou no App 'Tipo de Atividade'</p></body></html>")
 
 def listar(request):
     return HttpResponse("Lista de Tipos de Atividade")
     
+def show_mensagem(request):
+    x = "A"
+    nome = x + "ugusto, tudo bem ?"
+    return HttpResponse(f"Bom dia ! {nome}")
