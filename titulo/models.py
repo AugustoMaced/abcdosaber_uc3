f7819894dc4dff9a3da3a4dcece06f36a9202eb9
Augusto Maced <macedo271203@gmail.com>
2025-02-07 18:44:09 -0300

    continuando a modelagem

--- titulo/models.py
+++ titulo/models.py
@@ -7,7 +7,7 @@ class Titulo(models.Model):
     codigo = models.IntegerField(primary_key=True,
                                  help_text='Código do tipo de atividade')
     descricao= models.CharField(max_length=70, null=False,
-                                help_text='Informe a descrição do tipo de atividade')
+                                help_text='Informe a descrição do tipo dO TITULO')
     
     def __str__(self):
         return f'{self.codigo} - {self.descricao}'
